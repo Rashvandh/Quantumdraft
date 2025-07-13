0287160c87a8b66b3de03e3a3e0ceacba2af132b
Rashvandh <arashvandh85@gmail.com>
2025-07-13 22:05:03 +0530

    Fix: bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def static_files(filename):
     return app.send_static_file(filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # use the PORT provided by Render
+    app.run(host='0.0.0.0', port=port, debug=True)
